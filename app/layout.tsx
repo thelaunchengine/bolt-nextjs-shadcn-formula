f7d0fd154d12a4e7f2ed326ea1aece8580d376ea
thelaunchengine <dhaval@thelaunchengine.com>
2024-10-25 00:03:33 +0530

    title changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Whale Occurence Risk Level',
+  title: 'Whale Occurrence Risk Level',
   description: '',
 };
 
